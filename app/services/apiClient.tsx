eda60f38300235d8fc40c318d64c6d91d043acca
morakot maneelart <dev.morakot@gmail.com>
2026-03-13 21:47:30 +0700

    update env

--- app/services/apiClient.tsx
+++ app/services/apiClient.tsx
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:4000";
+const BASE_URL = "https://lotto-special-services.onrender.com";
 //https://lotto-special-services.onrender.com
 //http://localhost:4000
 async function apiRequest<T = unknown>(
